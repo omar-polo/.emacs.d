0452b9e1e450b74b801bfc830a594b4775c10abc
Omar Polo <op@omarpolo.com>
2020-12-08 17:02:58 +0100

    guard menu-bar-mode & friends

--- early-init.el
+++ early-init.el
@@ -1,10 +1,14 @@
 ;; -*- lexical-binding: t -*-
 
 ;; disable bloat
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(when (fboundp 'set-scroll-bar-mode)
-  (set-scroll-bar-mode nil))
+(when (fboundp 'menu-bar-mode)
+  (menu-bar-mode -1))
+(when (fboundp 'tool-bar-mode)
+  (tool-bar-mode -1))
+(when (fboundp 'scroll-bar-mode)
+  (scroll-bar-mode -1))
+(when (fboundp 'horizontal-scroll-bar-mode)
+  (horizontal-scroll-bar-mode -1))
 
 (defvar my/default-gc-cons-threshold gc-cons-threshold
   "Backup of the default GC threshold")
